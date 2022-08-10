21907bf12522c41074cd9545458d8cce24c70ab3
aman-alem <alem.aman@gmail.com>
2022-08-09 22:32:58 -0500

    listRoutes.js added and setup

---
+++ routes/listRoutes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const listCtrl = require("../controllers/");
+
+// index
+router.get("/", listCtrl.index);
+
+// create
+router.post("/", listCtrl.create);
+
+// update
+router.put("/:id", listCtrl.update);
+
+// delete
+router.delete("/:id", listCtrl.delete);
+
+module.exports = router;
