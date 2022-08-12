54242ebb7b7942a2b3e2dd3f1346c22bb5d30453
aman-alem <alem.aman@gmail.com>
2022-08-12 14:07:58 -0500

    set up cors, cookie-parser middlewares

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require("path");
 const logger = require("morgan");
 const userRoutes = require("./routes/userRoutes");
 const listRoutes = require("./routes/listRoutes");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -13,6 +15,8 @@ require("./db/connection");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
 
 // app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
 // app.use(express.static(path.join(__dirname, 'build')))
