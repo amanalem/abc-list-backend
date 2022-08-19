40702aabab0058abf7f33c452544b26283d9f690
aman-alem <alem.aman@gmail.com>
2022-08-18 23:17:55 -0500

    got rid of all old A/b/c code-- sticking to one item model, and a sub item model for future use

--- controllers/listCtrl.js
+++ controllers/listCtrl.js
@@ -1,7 +1,3 @@
-const A = require("../models/A");
-const B = require("../models/B");
-const C = require("../models/C");
-
 const cron = require("node-cron");
 const Item = require("../models/Item");
 
@@ -19,20 +15,6 @@ const index = async (req, res) => {
 
   let items = await Item.find({ owner: req.query.owner });
   res.json(items);
-
-  // let aItems = await A.find({ owner: req.query.owner });
-
-  // let bItems = await B.find({ owner: req.query.owner });
-
-  // let cItems = await C.find({ owner: req.query.owner });
-
-  // let list = {
-  //   aList: aItems,
-  //   bList: bItems,
-  //   cList: cItems,
-  // };
-
-  // res.json(list);
 };
 
 const create = (req, res) => {
@@ -43,21 +25,6 @@ const create = (req, res) => {
   }).then((item) => {
     res.json(item);
   });
-  // if (req.body.letter === "a") {
-  //   A.create({ entry: req.body.entry, owner: req.body.owner }).then(() => {
-  //     res.json("A item CREATED");
-  //   });
-  // }
-  // if (req.body.letter === "b") {
-  //   B.create({ entry: req.body.entry, owner: req.body.owner }).then(() => {
-  //     res.json("B item CREATED");
-  //   });
-  // }
-  // if (req.body.letter === "c") {
-  //   C.create({ entry: req.body.entry, owner: req.body.owner }).then(() => {
-  //     res.json("C item CREATED");
-  //   });
-  // }
 };
 
 const update = async (req, res) => {
@@ -67,50 +34,11 @@ const update = async (req, res) => {
     letter: req.body.letter,
   });
   res.json(item);
-  // if (req.params.letter.toLowerCase() === "a") {
-  //   A.findByIdAndUpdate(
-  //     req.params.id,
-  //     { entry: req.body.entry },
-  //     { new: true }
-  //   ).then(() => {
-  //     res.json("aList Item UPDATE");
-  //   });
-  // }
-  // if (req.params.letter.toLowerCase() === "b") {
-  //   B.findByIdAndUpdate(
-  //     req.params.id,
-  //     { entry: req.body.entry },
-  //     { new: true }
-  //   ).then(() => {
-  //     res.json("bList Item UPDATE");
-  //   });
-  // }
-  // if (req.params.letter.toLowerCase() === "c") {
-  //   C.findByIdAndUpdate(
-  //     req.params.id,
-  //     { entry: req.body.entry },
-  //     { new: true }
-  //   ).then(() => {
-  //     res.json("cList Item UPDATE");
-  //   });
-  // }
 };
 
 const deleteItem = async (req, res) => {
   await Item.findByIdAndDelete(req.params.id);
   res.json("Item deleted");
-  // if (req.params.letter.toLowerCase() === "a") {
-  //   await A.findByIdAndDelete(req.params.id);
-  //   res.json("aList item DELETED");
-  // }
-  // if (req.params.letter.toLowerCase() === "b") {
-  //   await B.findByIdAndDelete(req.params.id);
-  //   res.json("bList item DELETED");
-  // }
-  // if (req.params.letter.toLowerCase() === "c") {
-  //   await C.findByIdAndDelete(req.params.id);
-  //   res.json("cList item DELETED");
-  // }
 };
 
 module.exports = {
